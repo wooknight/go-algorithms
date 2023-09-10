45ce5d90538dfd722b78b5881c0799944d589c94
Ramesh Naidu <rameshnaidu@gmail.com>
2023-09-09 21:39:39 -0700

    bubble-sort

---
+++ bubble-sort/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func makeRandomSlice(numItems, max int) []int {
+	slice := make([]int, numItems)
+	for i := 0; i < numItems; i++ {
+		slice[i] = rand.Intn(max)
+	}
+	return slice
+}
+
+// Print at most numItems items.
+func printSlice(slice []int, numItems int) {
+	for i := 0; i < len(slice) && i < numItems; i++ {
+		fmt.Printf("%d ", slice[i])
+	}
+	fmt.Println()
+}
+
+// Verify that the slice is sorted.
+func checkSorted(slice []int) {
+	for i := 0; i < len(slice)-1; i++ {
+		if slice[i] > slice[i+1] {
+			fmt.Println("The slice is NOT sorted!")
+			return
+		}
+	}
+	fmt.Println("The slice is sorted!")
+}
+
+// Use bubble sort to sort the slice.
+func bubbleSort(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		for j := 0; j < len(slice)-1; j++ {
+			if slice[j] > slice[j+1] {
+				slice[j], slice[j+1] = slice[j+1], slice[j]
+			}
+		}
+	}
+}
+
+func main() {
+	slice := makeRandomSlice(100000, 100000)
+	fmt.Println("Initial slice of random numbers:")
+	printSlice(slice, 10)
+	bubbleSort(slice)
+	fmt.Println("Sorted slice:")
+	printSlice(slice, 10)
+	checkSorted(slice)
+}
